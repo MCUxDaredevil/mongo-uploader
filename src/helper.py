922cea958d97e9e90b848d7657485dc7f621dc4f
Aryan Donga (MCUxDaredevil) <daryan6680@proton.me>
2023-09-19 03:18:18 +0530

    Add file logging to helper

--- src/helper.py
+++ src/helper.py
@@ -2,22 +2,26 @@ import logging
 import sys
 from os import getenv
 
+from dotenv import load_dotenv
 from pymongo import MongoClient
+from pymongo.database import Database
 from pymongo.errors import ConfigurationError, ConnectionFailure, InvalidURI
 
+load_dotenv(override=True)
+
 logging.basicConfig(
-    format="%(asctime)s | %(filename)s:%(lineno)s >>> %(message)s",
+    format="\n\n%(asctime)s | %(filename)s:%(lineno)s >>> %(message)s",
     datefmt="%d-%m-%Y %H:%M:%S",
     level="DEBUG",
     filemode="a",
     filename="error.log",
 )
 
-logging.warning("Something bad is going to happen")
 
-
-def connect_to_db(db_name: str):
+def connect_to_db(db_name: str) -> tuple[MongoClient, Database]:
+    """Connect to the database and return the client and the database"""
     try:
+        print(getenv("MONGO_URI"))
         client = MongoClient(getenv("MONGO_URI"))
     except InvalidURI as err:
         print(f"Invalid URI provided! \n {err}")
@@ -29,14 +33,11 @@ def connect_to_db(db_name: str):
         print(f"Connection failure, Server Unavailable! \n {err}")
         sys.exit(1)
     except Exception as err:
-        print(f"Unknown error! Please contact the devs here https://github.com/MCUxDaredevil/mongo-uploader \n\n {err}")
+        print("Unknown error! Please contact the devs - https://github.com/MCUxDaredevil/mongo-uploader/issues")
+        print("You can find the log of this error in the error.log file\n")
+
         if getenv("LOGGING") == "True":
-            send_log(err)
+            logging.exception(err)
         sys.exit(1)
     else:
         return client, client[db_name]
-
-
-def send_log(err):
-    # TODO: Implement logging
-    pass
